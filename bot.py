1f664b3d7d4c4046554ec9aa0120a5225b8e5110
Isael.R.A <ir91146559@gamil.com>
2023-05-16 22:09:02 -0300

    Adicionado Opçoes 1 e 2

--- bot.py
+++ bot.py
@@ -1,14 +1,37 @@
 import telebot
+import requests
+import json
 
 CHAVE_API = ''
 bot = telebot.TeleBot(CHAVE_API)
 
-def verificar(messagem):
+@bot.message_handler(commands=['opcao1'])
+def responder(mensagem):
+
+    requisicao= requests.get(' https://economia.awesomeapi.com.br/last/USD-BRL,BTC-BRL,ETH-BRL')
+    moedas = requisicao.json()
+
+    usd = float(moedas["USDBRL"]["bid"])
+    btc = float(moedas["BTCBRL"]["bid"])
+    eth = float(moedas["ETHBRL"]["bid"])
+    bot.send_message(mensagem.chat.id, f'Cotações de Hoje:\n\n Dolar: R${usd:,.2f}\n Biticoin: R${btc:,.2f}\n Ethereum: R${eth:,.2f}')
+
+@bot.message_handler(commands=['opcao2'])
+def responder(mensagem):
+    bot.send_message(mensagem.chat.id, "Obrigado pelo abraço! Tenha um ótimo dia.")
+
+
+def verificar(mensagem):
     return True
 
 @bot.message_handler(func=verificar)
-def responder(messagem):
-    bot.reply_to(messagem, 'Olá aqui é o Bot do Isael')
+def responder(mensagem):
+    texto = """
+Escolha uma opção para continuar(Clique em uma Opção):
+/opcao1 Ver cotação do BTC,USD,ETH
+/opcao2 Mandar um  Abraço pro isael
+Responder qualquer outra coisa não vai funcionar, clique em uma das opções"""
+    bot.reply_to(mensagem, texto)
 
 
 
